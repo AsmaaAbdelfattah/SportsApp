9b9d65d090c36b59b7a2ab87f4877837ea18efe2
ZeinabMohamed <zm4816035@gmail.com>
2023-02-03 13:49:34 -0800

    createLeaguesClass

---
+++ SportsApp_ITI/ViewController/New Group/LeagusData.swift
@@ -0,0 +1,19 @@
+//
+//  LeagusData.swift
+//  SportsApp_ITI
+//
+//  Created by Zeinab on 03/02/2023.
+//
+
+import Foundation
+
+class LeguesData : Decodable {
+    var success : Int
+    var result : [Leagus]
+}
+
+class Leagus : Decodable {
+    var league_key : Int
+    var league_name : String
+    var league_logo : String?
+}
